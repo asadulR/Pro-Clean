b6706cf3daf888f93e68ef2c0b84015164caf60d
asadulR <asad1252001@gmail.com.com>
2022-05-15 10:32:13 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <div>
-      <h1 className=' text-red-700'>Clean kori </h1>
-      <button className='btn'>jhgf</button>
-    </div>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/services' element={<Services/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<Login/>}></Route>
+        </Routes>
+      </Navbar>
+      
+    </>
   );
 }
 
