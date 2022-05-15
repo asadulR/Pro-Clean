cf6cef50a7b2f98bb4feb853608f48831f11e8b1
asadulR <asad1252001@gmail.com.com>
2022-05-15 14:17:51 +0600

    landing page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Services from './pages/Services';
+import { publicRoute } from './Route/publicRoutes';
+
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 function App() {
   return (
     <>
       <Navbar>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
+          {/* <Route path='/' element={<Home/>}></Route>
           <Route path='/services' element={<Services/>}></Route>
           <Route path='/about' element={<About/>}></Route>
           <Route path='/contact' element={<Contact/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
-          <Route path='/signup' element={<Login/>}></Route>
+          <Route path='/signup' element={<Login/>}></Route> */}
+
+          {
+            publicRoute.map(({path, Component}, index) => <Route key={index} path={path} element={<Component/>}></Route>)
+          }
         </Routes>
       </Navbar>
       
